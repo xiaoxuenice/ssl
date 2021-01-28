1c01e8eecca9e15e2103533d2b6c5a0ce1b948d9
zhangsfdsaf <xiaoxuenice@qq.com>
2021-01-28 15:02:56 +0800

    ac安装,shengqin申请

--- acme1.sh
+++
@@ -1,2 +0,0 @@
-for i in `cat yuming.txt`;do  /root/.acme.sh/acme.sh --issue  -d "*.${i}" -d ${i}  --dns  --yes-I-know-dns-manual-mode-enough-go-ahead-please ;done
-
--- done3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#cat a.txt |grep Domain|awk -F "[']" '{print $2}'
-#cat a.txt |grep "TXT value"|awk -F "[']" '{print $2}'
-cd /root/.acme.sh
-rm -rf /shangchuan/*
-for i in `ls |grep \*`;do cp ${i}/${i}.cer ${i}/${i}.key /shangchuan ;done
-cd /shangchuan
-tar zcf yuming.tar.gz *
-scp yuming.tar.gz 192.168.1.12:/newzhengshu
